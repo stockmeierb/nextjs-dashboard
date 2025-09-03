c5ad3cc3d0d213faa32605c637694661861bbd15
Brett Stockmeier <stockmeierb@gmail.com>
2025-09-03 14:29:01 -0500

    ch 4 creating the dashboard page

--- app/page.tsx
+++ app/page.tsx
@@ -34,7 +34,7 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-                <Image
+        <Image
         src="/hero-desktop.png"
         width={1000}
         height={760}
